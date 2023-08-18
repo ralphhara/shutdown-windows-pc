da9da1aa1ccf11f1737c8e3cac968434fb99f126
Ralph Hara <ralph.naoto.hara@kyndryl.com>
2023-08-18 16:57:16 -0300

    Formatted the code

--- shutdown-windows-pc.py
+++ shutdown-windows-pc.py
@@ -5,6 +5,7 @@ def shutdown_after_minutes(minutes):
     seconds = minutes * 60
     time.sleep(seconds)
     os.system("shutdown /s /f /t 1")
+    
 print("*********************************************************************************")
 print("* This script will shutdown your computer after the amount of time you define.  *")
 print("* Please enter how long time (in minutes) you want to wait before the shutdown. *")
